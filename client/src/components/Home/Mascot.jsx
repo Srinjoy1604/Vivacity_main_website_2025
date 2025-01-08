3cf1a64112c1b6b3885b511c8e3db09d5ea6f816
aishwarya10112003 <145376808+aishwarya10112003@users.noreply.github.com>
2025-01-08 21:57:03 +0530

    mascot page updated

--- client/src/components/Home/Mascot.jsx
+++ client/src/components/Home/Mascot.jsx
@@ -8,17 +8,17 @@ function Mascot()
     return(
         <div className={`w-[100%] h-[100%] flex items-center align-middle justify-center ${styles.MascotBackground} max-[800px]:h-fit p-[2%]`}>
             <div className="w-[100%] grid grid-cols-2 place-items-center max-[800px]:grid-cols-1">
-                <div className="w-[100%] ">
-                    <div className={`${styles.NetraDiv} p-[10%] w-[100%] `}>
-                        <h1 className="text-center m-[1%] text-[#FFFFFF] font-rfabb text-[400] text-[2rem] max-[800px]:text-[1.25rem]">MEET OUR MASCOT</h1>
-                        <div className="w-[100% grid place-items-center"><img src={NetraText} className="w-[70%] h-auto"></img></div>
+                <div className="w-[100%] py-[2%] ">
+                    <div className={`${styles.NetraDiv} p-[6%] mb-[1%] pb-[10%] w-[100%] `}>
+                        <h1 className="text-center m-[1%] mt-0 text-[#FFFFFF] font-rfabb text-[400] text-[2rem] max-[800px]:text-[1.5rem]">MEET OUR MASCOT</h1>
+                        <div className="w-[100% grid place-items-center"><img src={NetraText} className="w-[70%] h-auto max-[800px]:w-[50%]"></img></div>
                     </div>
                     <div className={`${styles.NetraMobile} grid place-items-center w-[100%] h-[100%] p-[2.5%] hidden max-[800px]:block`}>
                         <img src={NetraMobile} className={`w-[100%] h-[100%]`}></img>
                     </div>
-                    <div className={`${styles.NetraOriginDiv} p-[10%] w-[100%]`}>
-                        <h1 className="text-white text-[2rem] font-rfabb m-[2%] max-[800px]:text-[1.5rem]">want to know the origin story of netra?</h1>
-                        <div className="w-[100%] h-[100%]"><button className={`${styles.NetraButton} p-[2.5%] w-fit text-black text-[400] text-[2rem] font-rfabb text-center max-[800px]:text-[1.25rem]`}>CHRONICLES OF NETRA</button></div>
+                    <div className={`${styles.NetraOriginDiv} p-[5%] w-[100%]`}>
+                        <h1 className="text-white text-[2rem] font-rfabb m-[2%] mb-[6%] max-[800px]:text-[1.5rem]">want to know the origin story of netra?</h1>
+                        <div className="w-[100%] h-[100%] pl-[2%] "><button className={`${styles.NetraButton} p-[2.5%] pl-[5%] pr-[5%] w-fit text-black text-[400]  text-[2rem] font-rfabb text-center max-[800px]:text-[1.25rem] `}>CHRONICLES OF NETRA</button></div>
                     </div>
                 </div>
                 <div className="w-[100%] h-[100%] max-[800px]:hidden">
